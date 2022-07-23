1dfd4ab3a6ce1d54a3a903042c17bc9a73a443de
MateusGPe <mateus.pereira1@aluno.ifsp.edu.br>
2022-07-23 03:08:40 -0300

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,50 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import makeManifest from './utils/plugins/make-manifest';
+import copyContentStyle from './utils/plugins/copy-content-style';
+import AutoImport from "unplugin-auto-import/vite";
+
+const root = resolve(__dirname, 'src');
+const pagesDir = resolve(root, 'pages');
+const assetsDir = resolve(root, 'assets');
+const outDir = resolve(__dirname, 'extension');
+const publicDir = resolve(__dirname, 'public');
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@src": root,
+      "@assets": assetsDir,
+      "@pages": pagesDir,
+    },
+  },
+  plugins: [react(), makeManifest(), copyContentStyle(),
+  AutoImport({
+    imports: [
+      {
+        "webextension-polyfill": [["default", "browser"]],
+      },
+    ],
+    dts: resolve(root, "auto-imports.d.ts"),
+  })],
+  publicDir,
+  build: {
+    outDir,
+    sourcemap: process.env.__DEV__ === "true",
+    rollupOptions: {
+      input: {
+        //devtools: resolve(pagesDir, 'devtools', 'index.html'),
+        //panel: resolve(pagesDir, 'panel', 'index.html'),
+        //content: resolve(pagesDir, 'content', 'index.ts'),
+        background: resolve(pagesDir, 'background', 'index.ts'),
+        popup: resolve(pagesDir, 'popup', 'index.html'),
+        //newtab: resolve(pagesDir, 'newtab', 'index.html'),
+        options: resolve(pagesDir, 'options', 'index.html'),
+      },
+      output: {
+        entryFileNames: (chunk) => `src/pages/${chunk.name}/index.js`,
+      },
+    },
+  },
+});
